fad8a43d76c577e12b71315f8a97742aabc65ab7
Niklasef <Niklasef@users.noreply.github.com>
2026-09-03 22:20:13 +0200

    Add gomoku solution (Sln + Gomoku project)

---
+++ gomoku/Stone.cs
@@ -0,0 +1,12 @@
+namespace Gomoku;
+
+public enum Stone
+{
+    Black = 0,
+    White = 1
+}
+
+public static class StoneUtils
+{
+    public static Stone Opponent(Stone s) => s == Stone.Black ? Stone.White : Stone.Black;
+}
