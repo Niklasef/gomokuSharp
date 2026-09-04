0a6e698e7ba6f4272698f1521700f9bbdc01d24e
Niklasef <Niklasef@users.noreply.github.com>
2026-09-04 16:39:00 +0200

    move entity

---
+++ gomoku/Move.cs
@@ -0,0 +1,15 @@
+namespace Gomoku;
+
+public readonly record struct Move
+{
+    public Stone Stone { get; }
+    public int X { get; }
+    public int Y { get; }
+
+    public Move(Stone stone, int x, int y)
+    {
+        Stone = stone;
+        X = x;
+        Y = y;
+    }
+}
